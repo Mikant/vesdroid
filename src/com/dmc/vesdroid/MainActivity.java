7d018ed6544e6c117037a256e6511fb6ba4785df
r00t3g <alexey.zarubin@gmail.com>
2013-09-16 18:47:20 +0400

    Primitive styling and base layouts

--- src/com/dmc/vesdroid/MainActivity.java
+++ src/com/dmc/vesdroid/MainActivity.java
@@ -2,14 +2,14 @@ package com.dmc.vesdroid;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		setContentView(R.layout.activity_main);
 	}
 
